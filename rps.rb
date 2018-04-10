7d457d80f3da4121e414840fecf23e256ec8ef36
bpierce <bpierce@ad25l300jdncr.78m460.nycboe.org>
2018-04-10 10:57:03 -0400

    working

--- rps.rb
+++ rps.rb
@@ -1,2 +1,19 @@
 # Rock / Paper / Scissors game
 
+def roll
+
+	computer_hand = "rock"
+
+	puts "Rock, paper, or scissors?"
+	user_hand = gets.chomp()
+
+	return [computer_hand , user_hand]
+end
+
+play = roll
+
+if play[0] == play[1]
+	print("same\n")
+else
+	print("different\n")
+end
