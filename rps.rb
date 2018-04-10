09a1a697279f45e718596a07487e2bf110c2f7e9
gdebord <bpie.nyc@gmail.com>
2018-04-10 14:17:30 -0400

    inelegant algorithm functional

--- rps.rb
+++ rps.rb
@@ -1,9 +1,11 @@
 # Rock / Paper / Scissors game
 
+Kernel#srand
+
 game_status = true
 
 def roll
-	computer_hand = "rock"
+	computer_hand = ["rock", "paper", "scissors"][rand(3)]
 
 	puts "Rock, paper, or scissors?"
 	user_hand = gets.chomp
@@ -15,6 +17,7 @@ end
 while (game_status)
 	play = roll
 
+	# user played rock
 	if play[1].downcase == "rock"
 		puts "user played rock\n"
 		if play[0].downcase == "rock" #play again
@@ -27,13 +30,41 @@ while (game_status)
 			puts "computer played scissors; user wins\n"
 			game_status = false
 		else
-			puts "user input error"
-			game_status = true
+			puts "error -- should not arrive here\n"
 		end
+	# user played paper
 	elsif play[1].downcase == "paper"
 		puts "user played paper\n"
+		if play[0].downcase == "paper" #play again
+			puts "computer played paper; play again\n"
+			game_status = true
+		elsif play[0].downcase == "rock"
+			puts "computer played rock; user wins\n"
+			game_status = false
+		elsif play[0].downcase == "scissors"
+			puts "computer played scissors; computer wins\n"
+			game_status = false
+		else
+			puts "error -- should not arrive here\n"
+		end
+	# user played scissors
 	elsif play[1].downcase == "scissors"
 		puts "user played scissors\n"
+		if play[0].downcase == "scissors" #play again
+			puts "computer played scissors; play again\n"
+			game_status = true
+		elsif play[0].downcase == "paper"
+			puts "computer played paper; user wins\n"
+			game_status = false
+		elsif play[0].downcase == "rock"
+			puts "computer played rock; computer wins\n"
+			game_status = false
+		else
+			puts "error -- should not arrive here\n"
+		end
+	else
+		puts "user input error"
+		game_status = true
 	end
 end
 
