caf2f4f90644908d6ff68c63f9737d7639079015
bpierce <bpierce@ad25l300jdncr.78m460.nycboe.org>
2018-04-20 10:37:17 -0400

    fixed bunch of stuff

--- sunrise.rb
+++ sunrise.rb
@@ -1,21 +1,51 @@
 require "net/http"
+require 'json'
 
-url = "https://api.sunrise-sunset.org/json?lat=40.7128&lng=74.0060&formatted=0"
+# Notes: almost working. Using formatted data, UTC hours show up as 1 or 2 digits, depending. Use unformatted? Parse on : ?
 
+# build url
+url = "https://api.sunrise-sunset.org/json?lat=40.7283&lng=-73.9472"
 uri = URI(url)
+
+# get json data
 response = Net::HTTP.get(uri)
-#response["results"]
-# #puts response
-# #puts response["results"]["sunrise"]
+
+# parse json data
 response_parsed = JSON.parse(response)
-hour = response_parsed["results"]["sunrise"][14...16].to_i
-hour = (hour - 4)%24
-response_parsed["results"]["sunrise"][14...16] = hour.to_s
 
-pp response_parsed["results"]["sunrise"]
+# sunrise
+# convert to Eastern Daylight Time, and print
+hour = response_parsed["results"]["sunrise"][0...2].to_i
+hour = (hour - 4)
+response_parsed["results"]["sunrise"][0...2] = hour.to_s
 
-# NOTE: the sunrise time is converting to Eastern, but it is in ISO 8601 format. That said, if the conversion goes to the previous day, the date needs to be changed as well
+print "Sunrise in Brooklyn is at:", response_parsed["results"]["sunrise"] , "\n"
 
+
+# sunset
+# convert to Eastern Daylight Time, and print
+hour = response_parsed["results"]["sunset"][0...2].to_i
+hour = (hour - 4)
+response_parsed["results"]["sunset"][0...2] = hour.to_s
+
+print "Sunset in Brooklyn is at:", response_parsed["results"]["sunset"] , "\n"
+
+# nautical twilight begins
+# convert to Eastern Daylight Time, and print
+print response_parsed["results"]["nautical_twilight_begin"]
+hour = response_parsed["results"]["nautical_twilight_begin"][0...2].to_i
+hour = (hour - 4)
+response_parsed["results"]["nautical_twilight_begin"][0...2] = hour.to_s
+
+print "Nautical twilight in Brooklyn begins at:", response_parsed["results"]["nautical_twilight_begin"] , "\n"
+
+# nautical twilight ends
+# convert to Eastern Daylight Time, and print
+hour = response_parsed["results"]["nautical_twilight_end"][0...2].to_i
+hour = (hour - 4)
+response_parsed["results"]["nautical_twilight_end"][0...2] = hour.to_s
+
+print "Nautical twilight in Brooklyn ends at:", response_parsed["results"]["nautical_twilight_end"] , "\n"
 
 
 
