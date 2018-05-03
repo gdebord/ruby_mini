e9e14512af06ba64e8d8df224c36370895e44578
gdebord <bpie.nyc@gmail.com>
2018-05-03 13:54:07 -0400

    fixed mod-12 and AM to PM issue for sunset and tw-end

--- sunrise.rb
+++ sunrise.rb
@@ -10,6 +10,8 @@ response = Net::HTTP.get(uri)
 
 # parse json data
 response_parsed = JSON.parse(response)
+print response_parsed # testing
+print "\n", response_parsed["results"]["nautical_twilight_end"][-2..-1] , "\n" #testing
 
 # print location
 print "******** Solar Events, Brooklyn EDT ********\n"
@@ -36,7 +38,11 @@ print "Sunrise is at: ", response_parsed["results"]["sunrise"] , "\n"
 # convert to Eastern Daylight Time, and print
 r_index = response_parsed["results"]["sunset"].index(':')
 hour = response_parsed["results"]["sunset"][0...r_index].to_i
-hour = (hour - 4)
+# sometimes sunset in UTC is the next day, so need to change AM to PM
+if (hour - 4) != (hour - 4) % 12
+	response_parsed["results"]["sunset"][-2..-1] = "PM"
+end
+hour = (hour - 4) % 12
 response_parsed["results"]["sunset"][0...r_index] = hour.to_s
 
 print "Sunset is at: ", response_parsed["results"]["sunset"] , "\n"
@@ -46,8 +52,14 @@ print "Sunset is at: ", response_parsed["results"]["sunset"] , "\n"
 # convert to Eastern Daylight Time, and print
 r_index = response_parsed["results"]["nautical_twilight_end"].index(':')
 hour = response_parsed["results"]["nautical_twilight_end"][0...r_index].to_i
-hour = (hour - 4)
+# sometimes twilight ends in UTC the next day, so need to change AM to PM
+if (hour - 4) != (hour - 4) % 12
+	response_parsed["results"]["nautical_twilight_end"][-2..-1] = "PM"
+end
+hour = (hour - 4) % 12
 response_parsed["results"]["nautical_twilight_end"][0...r_index] = hour.to_s
+
+
 
 print "Nautical twilight ends at: ", response_parsed["results"]["nautical_twilight_end"] , "\n"
 
